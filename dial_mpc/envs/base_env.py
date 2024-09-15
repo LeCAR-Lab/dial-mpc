95f1852092ec6e772794dcc14eae3cf8df560a58
Haoru Xue <haoru-xue@berkeley.edy>
2024-09-15 17:59:13 -0400

    implement base_env

---
+++ dial_mpc/envs/base_env.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import Any, Dict, Sequence, Tuple, Union, List
+from brax.base import System
+from brax.envs.base import PipelineEnv
+
+
+@dataclass
+class BaseEnvConfig:
+    task_name: str = 'default'
+    randomize_tasks: bool = False  # Whether to randomize the task.
+    # P gain, or a list of P gains for each joint.
+    kp: Union[float, List[float]] = 30.0
+    # D gain, or a list of D gains for each joint.
+    kd: Union[float, List[float]] = 1.0
+    debug: bool = False
+    # dt of the environment step, not the underlying simulator step.
+    dt: float = 0.02
+    timestep: float = 0.02  # timestep of the underlying simulator step.
+    backend: str = 'mjx'  # backend of the environment.
+
+
+class BaseEnv(PipelineEnv):
+    def __init__(self, config: BaseEnvConfig):
+        assert config.timestep % config.dt == 0, 'timestep must be divisible by dt'
+        self._config = config
+        n_frames = int(config.dt / config.timestep)
+        sys = self.make_system(config)
+        super().__init__(sys, config.backend, n_frames, config.debug)
+
+    def make_system(self, config: BaseEnvConfig) -> System:
+        """
+        Make the system for the environment. Called in BaseEnv.__init__.
+        """
+        raise NotImplementedError
